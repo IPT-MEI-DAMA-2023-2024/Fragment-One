596042767cf0ca66a0a7d4e088a49dbdb8229e7c
jcnpereira <casimiro@ipt.pt>
2024-04-04 21:30:19 +0100

    add the MainActivity action

--- app/src/main/java/pt/ipt/dama2024/fragmentone/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama2024/fragmentone/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // add reference to the fragments that we need to use
+    lateinit var f1:MyFragment
+    lateinit var f2:MyFragment
+    lateinit var f3:MyFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        f1= MyFragment.newInstance("Fragmento um","botão um")
+        f2= MyFragment.newInstance("Fragmento Dois","botão Dois")
+        f3= MyFragment.newInstance("Fragmento TRÊS","botão TRÊS")
+
+        // assign the fragments to the interface
+        val fragmentTransaction=supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.frag1,f1)
+        fragmentTransaction.add(R.id.frag2,f2)
+        fragmentTransaction.add(R.id.frag3,f3)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
+
     }
 }
